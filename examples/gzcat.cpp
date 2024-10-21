a1d286018a5db028ce9cb8fc491357eab8c17c16
Drew DeHaas <drewdehaas@gmail.com>
2024-10-21 09:01:18 -0400

    Update with .clang-format file to properly format the code
    Previous attempt was using defaults which were sub-optimal.

--- examples/gzcat.cpp
+++ examples/gzcat.cpp
@@ -9,18 +9,18 @@
 
 using namespace picovcf;
 
-int main(int argc, char *argv[]) {
-  if (argc < 1) {
-    std::cerr << "Please pass in an input file" << std::endl;
-    return 1;
-  }
+int main(int argc, char* argv[]) {
+    if (argc < 1) {
+        std::cerr << "Please pass in an input file" << std::endl;
+        return 1;
+    }
 
-  const std::string filename(argv[1]);
-  ZBufferedReader reader(filename, 128 * 1024);
-  std::string line;
-  while (reader.readline(line) > 0) {
-    std::cout << line << std::endl;
-  }
+    const std::string filename(argv[1]);
+    ZBufferedReader reader(filename, 128 * 1024);
+    std::string line;
+    while (reader.readline(line) > 0) {
+        std::cout << line << std::endl;
+    }
 
-  return 0;
+    return 0;
 }
--- examples/igdpp.cpp
+++ examples/igdpp.cpp
@@ -14,94 +14,90 @@
 
 using namespace picovcf;
 
-int main(int argc, char *argv[]) {
-  std::cout << std::fixed << std::setprecision(4);
-  if (argc < 3) {
-    std::cerr << "Please pass in a command and an input file (in that order)"
-              << std::endl;
-    return 1;
-  }
+int main(int argc, char* argv[]) {
+    std::cout << std::fixed << std::setprecision(4);
+    if (argc < 3) {
+        std::cerr << "Please pass in a command and an input file (in that order)" << std::endl;
+        return 1;
+    }
 
-  const std::string command(argv[1]);
-  const std::string filename(argv[2]);
+    const std::string command(argv[1]);
+    const std::string filename(argv[2]);
 
-  IGDData igd(filename);
+    IGDData igd(filename);
 
-  if (command == "stats") {
-    std::cout << "Stats for " << filename << std::endl;
-    std::cout << "  Variants: " << igd.numVariants() << std::endl;
-    std::cout << "  Individuals: " << igd.numIndividuals() << std::endl;
-    std::cout << "  Ploidy: " << igd.getPloidy() << std::endl;
-    std::cout << "  Phased?: " << (igd.isPhased() ? "true" : "false")
-              << std::endl;
-    std::cout << "  Source: " << igd.getSource() << std::endl;
-    std::cout << "  Genome range: " << igd.getPosition(0) << "-"
-              << igd.getPosition(igd.numVariants() - 1) << std::endl;
-    std::cout << "  Has individual IDs? "
-              << (igd.getIndividualIds().empty() ? "No" : "Yes") << std::endl;
-  } else if (command == "range_stats") {
-    std::cout << "Stats for " << filename << std::endl;
-    bool _ignore = false;
-    size_t firstPos = igd.getPosition(0, _ignore);
-    size_t lastPos = igd.getPosition(igd.numVariants() - 1, _ignore);
-    uint32_t start = (argc > 3) ? atoi(argv[3]) : firstPos;
-    uint32_t end = (argc > 4) ? atoi(argv[4]) : lastPos + 1;
-    size_t variants = 0;
-    size_t missingRows = 0;
-    size_t missingAlleles = 0;
-    std::vector<size_t> samplesPerVariant;
-    size_t sampleRefsTotal = 0;
-    std::vector<size_t> sampleToMuts(
-        igd.numSamples()); // Counts muts per sample
-    for (size_t i = 0; i < igd.numVariants(); i++) {
-      bool isMissing = false;
-      auto pos = igd.getPosition(i, isMissing);
-      if (pos >= start && pos < end) {
-        variants++;
-        auto sampleList = igd.getSamplesWithAlt(i);
-        if (isMissing) {
-          missingRows++;
+    if (command == "stats") {
+        std::cout << "Stats for " << filename << std::endl;
+        std::cout << "  Variants: " << igd.numVariants() << std::endl;
+        std::cout << "  Individuals: " << igd.numIndividuals() << std::endl;
+        std::cout << "  Ploidy: " << igd.getPloidy() << std::endl;
+        std::cout << "  Phased?: " << (igd.isPhased() ? "true" : "false") << std::endl;
+        std::cout << "  Source: " << igd.getSource() << std::endl;
+        std::cout << "  Genome range: " << igd.getPosition(0) << "-" << igd.getPosition(igd.numVariants() - 1)
+                  << std::endl;
+        std::cout << "  Has individual IDs? " << (igd.getIndividualIds().empty() ? "No" : "Yes") << std::endl;
+    } else if (command == "range_stats") {
+        std::cout << "Stats for " << filename << std::endl;
+        bool _ignore = false;
+        size_t firstPos = igd.getPosition(0, _ignore);
+        size_t lastPos = igd.getPosition(igd.numVariants() - 1, _ignore);
+        uint32_t start = (argc > 3) ? atoi(argv[3]) : firstPos;
+        uint32_t end = (argc > 4) ? atoi(argv[4]) : lastPos + 1;
+        size_t variants = 0;
+        size_t missingRows = 0;
+        size_t missingAlleles = 0;
+        std::vector<size_t> samplesPerVariant;
+        size_t sampleRefsTotal = 0;
+        std::vector<size_t> sampleToMuts(igd.numSamples()); // Counts muts per sample
+        for (size_t i = 0; i < igd.numVariants(); i++) {
+            bool isMissing = false;
+            auto pos = igd.getPosition(i, isMissing);
+            if (pos >= start && pos < end) {
+                variants++;
+                auto sampleList = igd.getSamplesWithAlt(i);
+                if (isMissing) {
+                    missingRows++;
+                }
+                for (auto sampleId : sampleList) {
+                    if (!isMissing) {
+                        sampleToMuts.at(sampleId)++;
+                    } else {
+                        missingAlleles++;
+                    }
+                }
+                auto sampleCt = sampleList.size();
+                samplesPerVariant.push_back(sampleCt);
+                sampleRefsTotal += sampleCt;
+            }
         }
-        for (auto sampleId : sampleList) {
-          if (!isMissing) {
-            sampleToMuts.at(sampleId)++;
-          } else {
-            missingAlleles++;
-          }
+        std::cout << "... in range " << start << " - " << end << std::endl;
+        std::cout << "  Variants in range: " << variants << std::endl;
+        const double avgSamples = (double)sampleRefsTotal / (double)variants;
+        std::cout << "  Average samples/var: " << avgSamples << std::endl;
+        double stddevSamples = 0.0;
+        for (auto count : samplesPerVariant) {
+            double diff = (double)count - avgSamples;
+            stddevSamples += (diff * diff);
         }
-        auto sampleCt = sampleList.size();
-        samplesPerVariant.push_back(sampleCt);
-        sampleRefsTotal += sampleCt;
-      }
-    }
-    std::cout << "... in range " << start << " - " << end << std::endl;
-    std::cout << "  Variants in range: " << variants << std::endl;
-    const double avgSamples = (double)sampleRefsTotal / (double)variants;
-    std::cout << "  Average samples/var: " << avgSamples << std::endl;
-    double stddevSamples = 0.0;
-    for (auto count : samplesPerVariant) {
-      double diff = (double)count - avgSamples;
-      stddevSamples += (diff * diff);
-    }
-    stddevSamples = sqrt(stddevSamples / (double)variants);
-    std::cout << "  Stddev samples/var: " << stddevSamples << std::endl;
+        stddevSamples = sqrt(stddevSamples / (double)variants);
+        std::cout << "  Stddev samples/var: " << stddevSamples << std::endl;
 
-    size_t mutRefsTotal = 0;
-    for (size_t i = 0; i < sampleToMuts.size(); i++) {
-      mutRefsTotal += sampleToMuts[i];
-    }
-    const double avgMuts = (double)mutRefsTotal / (double)igd.numSamples();
-    std::cout << "  Average var/sample: " << avgMuts << std::endl;
-    double stddevMuts = 0.0;
-    for (size_t i = 0; i < sampleToMuts.size(); i++) {
-      double diff = (double)sampleToMuts[i] - avgMuts;
-      stddevMuts += (diff * diff);
-    }
-    stddevMuts = sqrt(stddevMuts / (double)igd.numSamples());
-    std::cout << "  Stddev var/sample: " << stddevMuts << std::endl;
+        size_t mutRefsTotal = 0;
+        for (size_t i = 0; i < sampleToMuts.size(); i++) {
+            mutRefsTotal += sampleToMuts[i];
+        }
+        const double avgMuts = (double)mutRefsTotal / (double)igd.numSamples();
+        std::cout << "  Average var/sample: " << avgMuts << std::endl;
+        double stddevMuts = 0.0;
+        for (size_t i = 0; i < sampleToMuts.size(); i++) {
+            double diff = (double)sampleToMuts[i] - avgMuts;
+            stddevMuts += (diff * diff);
+        }
+        stddevMuts = sqrt(stddevMuts / (double)igd.numSamples());
+        std::cout << "  Stddev var/sample: " << stddevMuts << std::endl;
 
-    std::cout << "  Variants with missing data: " << missingRows << std::endl;
-    std::cout << "  Total missing alleles: " << missingAlleles << std::endl;
-  }
-  return 0;
+        std::cout << "  Variants with missing data: " << missingRows << std::endl;
+        std::cout << "  Total missing alleles: " << missingAlleles << std::endl;
+    }
+    return 0;
 }
