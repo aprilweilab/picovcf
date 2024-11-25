f02ca966365ee0a277562cb28a41e804eed1fc08
Drew DeHaas <drewdehaas@gmail.com>
2024-11-25 15:40:13 -0500

    A more compact and useful representation for unphased data
    Previously, unphased data was stored just like phased data. The only
    difference was a flag set in the file header, otherwise you still had
    haploid sample lists that you need to traverse and count the number
    of copies based on the ploidy.
    
    Now we store the number of copies explicitly in the position index,
    where we reserve 8-bits for this (so supports ploidy up to 255).
    Each variant row then has an associated number of copies, and you
    can just skip a variant if it has a number of copies you don't
    care about. For example, with diploid data you can do analyses with
    just homozygotes by only scanning variants with numCopies=2, which
    would massively speed up your analysis.

--- test/test_igd.cpp
+++ test/test_igd.cpp
@@ -7,6 +7,7 @@
 using namespace picovcf;
 
 extern const std::string getMISSING_DATA_EXAMPLE_FILE();
+extern const std::string getUNPHASED_DATA_EXAMPLE_FILE();
 
 TEST(IGD, MissingData) {
     std::string igdFileName = "missing_data.igd";
@@ -26,4 +27,34 @@ TEST(IGD, MissingData) {
     }
     // Every variant is missing
     ASSERT_EQ(numMissing, 5);
+}
+
+TEST(IGD, UnphasedData) {
+    std::string igdFileName = "unphased_data.igd";
+    vcfToIGD(getUNPHASED_DATA_EXAMPLE_FILE(), igdFileName);
+
+    IGDData igdFile(igdFileName);
+
+    ASSERT_EQ(igdFile.numVariants(), 7);
+    size_t totalOnes = 0;
+    size_t totalTwos = 0;
+    for (size_t i = 0; i < igdFile.numVariants(); i++) {
+        bool isMissing = false;
+        uint8_t numCopies = 0;
+        igdFile.getPosition(i, isMissing, numCopies);
+        ASSERT_GT(numCopies, 0);
+        ASSERT_LE(numCopies, 2);
+        ASSERT_FALSE(isMissing);
+        auto sampleList = igdFile.getSamplesWithAlt(i);
+        for (const auto index : sampleList) {
+            ASSERT_LT(index, igdFile.numIndividuals());
+        }
+        if (numCopies == 1) {
+            totalOnes += sampleList.size();
+        } else if (numCopies == 2) {
+            totalTwos += sampleList.size();
+        }
+    }
+    ASSERT_EQ(totalOnes, 90+325+146+129);
+    ASSERT_EQ(totalTwos, 2+2+1);
 }
